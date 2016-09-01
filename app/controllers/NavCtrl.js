56bd537ee6dba13dfd71f0e22ced251e2524980e
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-01 11:01:42 -0500

    Created hide and show function for search bar

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,7 +1,9 @@
 "use strict";
 //Now we're adding a property on our app, which is a controller. The first argument is the name and the second is the syntax for a function. You NEED to add $scope inside of the function.
-app.controller("NavCtrl", function($scope, SearchTermData){
+app.controller("NavCtrl", function($scope, SearchTermData, $location){
     //Since the scope we're in is NavCtrl, we have access to navItems inside of the HTML where we've declared the ng-controller (which is inside the nav tags)
+    //$location is a predefined thing in Angular
+    $scope.isActive = (viewLocation) => viewLocation === $location.path();
     $scope.searchText = SearchTermData;
     $scope.navItems = [
         {name: "Logout", url: '#/logout'},
